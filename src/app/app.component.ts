b3c438032fccc623ba58469963bfbc9296973cb2
Philippe Lamorski <philippe.lamorskipro@gmail.com>
2024-04-02 17:47:53 +0200

    premier cas d'Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,30 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FaceSnapComponent } from "./face-snap/face-snap.component";
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
 import { HeaderComponent } from "./header/header.component";
+import { filter, interval, map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, FaceSnapComponent, FaceSnapListComponent,  HeaderComponent],
+    imports: [CommonModule, RouterOutlet, RouterLink, 
+        RouterLinkActive, FaceSnapComponent, FaceSnapListComponent,  HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent  implements OnInit {
 
+    interval$!: Observable<string>
+
     ngOnInit(): void {
+        this.interval$ = interval(1000).pipe(
+            filter(value => value % 3 === 0),
+            map(value => value % 2 === 0 ? 
+                'je suis '  + value +  ' et je suis pair' : 
+                'je suis ' + value + ' et je suis impair'
+                )
+        );
+
         
     }
 }
