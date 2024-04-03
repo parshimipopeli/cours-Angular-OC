e5615a794205fd0108f574e535a0c3fb0e079387
Philippe Lamorski <philippe.lamorskipro@gmail.com>
2024-04-03 09:13:43 +0200

    création d'un Observable dans face-snap-list.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,26 +21,26 @@ export class AppComponent implements OnInit {
     yellowTrainsCalled = 0;
 
     ngOnInit() {
-        interval(1000).pipe(
-            take(10),
-            map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
-            tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-            switchMap(color => this.getTrainObservable$(color)),
-            tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
-        ).subscribe();
-    }
+    //     interval(1000).pipe(
+    //         take(10),
+    //         map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+    //         tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+    //         switchMap(color => this.getTrainObservable$(color)),
+    //         tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    //     ).subscribe();
+    // }
 
-    getTrainObservable$(color: 'rouge' | 'jaune') {
-        const isRedTrain = color === 'rouge';
-        isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-        const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-        console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-        return of({ color, trainIndex }).pipe(
-            delay(isRedTrain ? 5000 : 6000)
-        );
-    }
+    // getTrainObservable$(color: 'rouge' | 'jaune') {
+    //     const isRedTrain = color === 'rouge';
+    //     isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    //     const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    //     console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    //     return of({ color, trainIndex }).pipe(
+    //         delay(isRedTrain ? 5000 : 6000)
+    //     );
+    // }
 
-    translateColor(color: 'rouge' | 'jaune') {
-        return color === 'rouge' ? 'red' : 'yellow';
+    // translateColor(color: 'rouge' | 'jaune') {
+    //     return color === 'rouge' ? 'red' : 'yellow';
     }
 }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -3,6 +3,7 @@ import { FaceSnapComponent } from "../face-snap/face-snap.component";
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 import { RouterLink } from '@angular/router';
+import { interval, Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
     selector: 'app-face-snap-list',
@@ -14,12 +15,22 @@ import { RouterLink } from '@angular/router';
 export class FaceSnapListComponent implements  OnInit {
 
     faceSnaps!: FaceSnap[];
+    private destroy$!: Subject<boolean>;
 
     constructor(private faceSnapsService: FaceSnapsService) {}
 
     ngOnInit(): void {
+        this.destroy$ = new Subject<boolean>();
         this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+        interval(1000).pipe(
+            takeUntil(this.destroy$),
+            tap(console.log)
+            ).subscribe();
     }
 
-  
+    ngOnDestroy() {
+        this.destroy$.next(true);
+    }
+
 }
